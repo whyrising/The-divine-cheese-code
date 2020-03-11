5556ed5e5c9466299274007cb3d2d250ddc06422
Yahya <yahyatinani@gmail.com>
2020-03-11 16:31:43 +0100

    Implement the cheese burglar finder

---
+++ src/the_divine_cheese_code/core.clj
@@ -0,0 +1,48 @@
+(ns the-divine-cheese-code.core
+  (:require [clojure.java.browse :as browse]
+            [the-divine-cheese-code.visualization.svg :refer [xml]]
+            [clojure.string :as s])
+  (:gen-class))
+
+(def heists [{:location    "Cologne, Germany"
+              :cheese-name "Archbishop Hildebold's Cheese Pretzel"
+              :lat         50.95
+              :lng         6.97}
+             {:location    "Zurich, Switzerland"
+              :cheese-name "The Standard Emmental"
+              :lat         47.37
+              :lng         8.55}
+             {:location    "Marseille, France"
+              :cheese-name "Le Fromage de Cosquer"
+              :lat         43.30
+              :lng         5.37}
+             {:location    "Zurich, Switzerland"
+              :cheese-name "The Lesser Emmental"
+              :lat         47.37
+              :lng         8.55}
+             {:location    "Vatican City"
+              :cheese-name "The Cheese of Turin"
+              :lat         41.90
+              :lng         12.45}])
+
+(defn url
+  "file:///C:/Users/whydr/src/clojure_projects/the-divine-cheese-code/map.html"
+  [filename]
+  (str "file:///"
+       (s/replace (System/getProperty "user.dir") "\\" "/")
+       "/"
+       filename))
+
+(defn template
+  [contents]
+  (str "<style>polyline { fill:none; stroke:#5881d8; stroke-width:3}</style>"
+       contents))
+
+(defn -main
+  [& args]
+  (let [filename "map.html"]
+    (->> heists
+         (xml 50 100)
+         template
+         (spit filename))
+    (browse/browse-url (url filename))))
